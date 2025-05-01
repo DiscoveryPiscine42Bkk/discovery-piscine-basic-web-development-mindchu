1a59242896f5eba0dbc226db52210d0e022005c9
Chuthathip Termchaikul <chutermc@z2t2c1.42bangkok.com>
2025-05-01 16:36:02 +0700

    3.2

---
+++ cell03/ex03/todo.js
@@ -0,0 +1,61 @@
+var task = [];
+document.addEventListener("DOMContentLoaded", getCookies);
+
+function addtask() {
+    let new_task = window.prompt("Add new to-do:");
+    if (new_task == null || new_task.trim() === "") {
+        alert("New to-do cannot be empty!");
+        return;
+    }
+
+    addTaskToUI(new_task);
+    task.push(new_task);
+    saveTasksToCookies();
+}
+
+function addTaskToUI(taskText) {
+    var taskItem = document.createElement("div");
+    var taskParagraph = document.createElement("p");
+    taskParagraph.textContent = taskText;
+    taskItem.appendChild(taskParagraph);
+
+    taskItem.addEventListener("click", function() {
+        removeTask(taskText, taskItem);
+    });
+
+    document.getElementById("ft_list").prepend(taskItem);
+}
+
+function removeTask(taskText, taskElement) {
+    var remove_task = confirm("Are you sure you want to remove this to-do?");
+    if (remove_task) {
+        const index = task.indexOf(taskText);
+        if (index > -1) {
+            task.splice(index, 1);
+            saveTasksToCookies();
+        }
+        taskElement.remove();
+    }
+}
+
+function getCookies() {
+    const theCookie = document.cookie;
+    if (theCookie) {
+        const cookies = theCookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.startsWith("tasks=")) {
+                const taskValues = cookie.substring("tasks=".length).split(',');
+                if(taskValues[0] !== ""){
+                    task = taskValues;
+                    task.forEach(addTaskToUI);
+                }
+                break;
+            }
+        }
+    }
+}
+
+function saveTasksToCookies() {
+    document.cookie = "tasks=" + task.join(',') + "; path=/";
+}
